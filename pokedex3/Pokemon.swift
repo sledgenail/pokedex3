5f9b28389f23c9c3c22ad1245cd0cd7bf7b0f8e2
Emmanuel Erilibe <sledgenail@gmail.com>
2016-11-11 00:33:48 -0600

    Re-adding the application

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Emmanuel Erilibe on 11/10/16.
+//  Copyright © 2016 Emmanuel Erilibe. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Emmanuel Erilibe on 11/10/16.
+//  Copyright © 2016 Emmanuel Erilibe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+    }
+
+
+
+}
+
