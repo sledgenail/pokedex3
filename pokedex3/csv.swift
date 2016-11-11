34c51d3cf9bc6a54788d14d32d223604259935d9
Emmanuel Erilibe <sledgenail@gmail.com>
2016-11-11 13:42:50 -0600

    Same stuff

--- pokedex3/csv.swift
+++ pokedex3/csv.swift
@@ -5,19 +5,19 @@
 
 import Foundation
 
-open class CSV {
-    open var headers: [String] = []
-    open var rows: [Dictionary<String, String>] = []
-    open var columns = Dictionary<String, [String]>()
-    var delimiter = CharacterSet(charactersIn: ",")
+public class CSV {
+    public var headers: [String] = []
+    public var rows: [Dictionary<String, String>] = []
+    public var columns = Dictionary<String, [String]>()
+    var delimiter = NSCharacterSet(charactersInString: ",")
     
-    public init(content: String?, delimiter: CharacterSet, encoding: UInt) throws{
+    public init(content: String?, delimiter: NSCharacterSet, encoding: UInt) throws{
         if let csvStringToParse = content{
             self.delimiter = delimiter
 
-            let newline = CharacterSet.newlines
+            let newline = NSCharacterSet.newlineCharacterSet()
             var lines: [String] = []
-            csvStringToParse.trimmingCharacters(in: newline).enumerateLines { line, stop in lines.append(line) }
+            csvStringToParse.stringByTrimmingCharactersInSet(newline).enumerateLines { line, stop in lines.append(line) }
 
             self.headers = self.parseHeaders(fromLines: lines)
             self.rows = self.parseRows(fromLines: lines)
@@ -26,32 +26,32 @@ open class CSV {
     }
     
     public convenience init(contentsOfURL url: String) throws {
-        let comma = CharacterSet(charactersIn: ",")
+        let comma = NSCharacterSet(charactersInString: ",")
         let csvString: String?
         do {
-            csvString = try String(contentsOfFile: url, encoding: String.Encoding.utf8)
+            csvString = try String(contentsOfFile: url, encoding: NSUTF8StringEncoding)
         } catch _ {
             csvString = nil
         };
-        try self.init(content: csvString,delimiter:comma, encoding:String.Encoding.utf8.rawValue)
+        try self.init(content: csvString,delimiter:comma, encoding:NSUTF8StringEncoding)
     }
     
     
     func parseHeaders(fromLines lines: [String]) -> [String] {
-        return lines[0].components(separatedBy: self.delimiter)
+        return lines[0].componentsSeparatedByCharactersInSet(self.delimiter)
     }
     
     func parseRows(fromLines lines: [String]) -> [Dictionary<String, String>] {
         var rows: [Dictionary<String, String>] = []
         
-        for (lineNumber, line) in lines.enumerated() {
+        for (lineNumber, line) in lines.enumerate() {
             if lineNumber == 0 {
                 continue
             }
             
             var row = Dictionary<String, String>()
-            let values = line.components(separatedBy: self.delimiter)
-            for (index, header) in self.headers.enumerated() {
+            let values = line.componentsSeparatedByCharactersInSet(self.delimiter)
+            for (index, header) in self.headers.enumerate() {
                 if index < values.count {
                     row[header] = values[index]
                 } else {
